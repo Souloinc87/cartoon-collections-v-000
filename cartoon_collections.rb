14326dfd7202b03556b152ff35d88b87e2e33037
Marcus  = Soulo <souloinc87@gmail.com>
2018-03-24 00:43:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|find| cheese_types.detect}
+  array.find {|find| cheese_types.include?(find)}
 end
